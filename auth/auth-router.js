d92c80134b57a499a3afd09510dafd41fcc37e59
Oluwajoba <oluwajobabello@gmail.com>
2019-11-14 04:34:45 +0100

    update auth-router.js post(/login)

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -17,3 +17,25 @@ router.post('/register', (req, res) => {
         res.status(500).json(error);
       });
   });
+
+  router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+            token: token,
+          });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
